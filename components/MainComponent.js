27f8cf9500487d87c70b1e31750bc14392e80e76
Kevin Driscoll <kpdriscoll6@gmail.com>
2020-09-15 13:03:08 -0600

    added redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,6 +9,25 @@ import { Icon } from 'react-native-elements';
 import { createStackNavigator, createDrawerNavigator,DrawerItems } from 'react-navigation';
 import SafeAreaView from 'react-native-safe-area-view';
 
+const CustomDrawerContentComponent = props => (
+    <ScrollView>
+        <SafeAreaView 
+            style={styles.container}
+            forceInset={{top: 'always', horizontal: 'never'}}>
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>
+                    <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+                </View>
+                <View style={{flex: 2}}>
+                    <Text style={styles.drawerHeaderText}>NuCamp</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+);
+
+
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory }
@@ -162,6 +181,7 @@ const MainNavigator = createDrawerNavigator(
     },
     {
         drawerBackgroundColor: '#CEC8FF',
+        contentComponent: CustomDrawerContentComponent
     }
 )
 class Main extends Component {
@@ -178,6 +198,27 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
