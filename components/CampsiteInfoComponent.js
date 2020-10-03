29bb6492ae062393cfb6eee3c731b7ae765f55da
Kevin Driscoll <kpdriscoll6@gmail.com>
2020-10-03 08:08:39 -0600

    added email and social buttons

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -21,6 +21,17 @@ const mapDispatchToProps = {
     postFavorite: campsiteId => (postFavorite(campsiteId)),
     postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text))
 }
+
+
+const shareCampsite = (title, message, url) => {
+    Share.share({
+        title: title,
+        message: `${title}: ${message} ${url}`,
+        url: url
+    },{
+        dialogTitle: 'Share ' + title
+    });
+};
 
 function RenderCampsite(props) {
 
@@ -97,6 +108,15 @@ function RenderCampsite(props) {
                     reverse
                     onPress={() => props.onShowModal()}
                 />
+                <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            style={styles.cardItem}
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
+                        />
                 </View>
             </Card>
             </Animatable.View>
